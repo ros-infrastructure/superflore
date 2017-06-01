18e3f1c1557932b09dd609fc3f6e506ed5a8ef14
Hunter L. Allen <hunter@openrobotics.org>
2017-06-01 09:53:59 -0700

    Almost filing the pull requests now.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Ebuild Generator:
 =================
 This is a python module for generating Gentoo Linux
 ebuilds.
+
+Dependencies:
+--------------
+You may use either Python 2 or Python 3, tested with 2.7, 3.4 and 3.5.
+
+Please install the following `pip` packages:
+```
+pip install git-pull-request xmltodict termcolor rosdistro rosinstall_generator gitpython --user
+```
+
--- run.py
+++ run.py
@@ -2,9 +2,27 @@
 from scripts.gen_packages import generate_installers
 from scripts.overlay_instance import ros_overlay
 import shutil
+import os
 
 # clone current repo
 overlay = ros_overlay()
+
+def link_existing_files():
+    global overlay
+    ros_overlay.info('Symbolicly linking files from {0}...'.format(overlay.repo_dir + '/ros-indigo'))
+    os.symlink(overlay.repo_dir + '/ros-indigo', './ros-indigo')
+    ros_overlay.info('Symbolicly linking files from {0}...'.format(overlay.repo_dir + '/ros-kinetic'))
+    os.symlink(overlay.repo_dir + '/ros-kinetic', './ros-kinetic')
+    ros_overlay.info('Symbolicly linking files from {0}...'.format(overlay.repo_dir + '/ros-lunar'))
+    os.symlink(overlay.repo_dir + '/ros-lunar', './ros-lunar')
+
+try:
+    link_existing_files()
+except FileExistsError as fe:
+    ros_overlay.warn('Detected existing rosdistro ebuil structure... Removing and overwriting.')
+    for x in [ 'ros-lunar', 'ros-kinetic', 'ros-indigo' ]:
+        os.remove(x)
+        os.symlink(overlay.repo_dir + '/' + x, './' + x) 
 
 # generate installers for kinetic
 indigo_installers, indigo_broken = generate_installers("indigo")
@@ -12,13 +30,6 @@ kinetic_installers, kinetic_broken = generate_installers("kinetic")
 lunar_installers, lunar_broken = generate_installers("lunar")
 
 master_list = lunar_broken.copy()
-
-ros_overlay.info('Copying files from {0}...'.format(overlay.repo_dir + '/ros-indigo'))
-shutil.copytree(overlay.repo_dir + '/ros-indigo', './')
-ros_overlay.info('Copying files from {0}...'.format(overlay.repo_dir + '/ros-kinetic'))
-shutil.copytree(overlay.repo_dir + '/ros-kinetic', './')
-ros_overlay.info('Copying files from {0}...'.format(overlay.repo_dir + '/ros-lunar'))
-shutil.copytree(overlay.repo_dir + '/ros-lunar', './')
 
 for p in kinetic_broken.keys():
     if p not in master_list.keys():
@@ -46,10 +57,10 @@ for pkg in inst_list:
 overlay.clean_ros_ebuild_dirs()
 
 # Copy ros-* to ebuild directory
-shutil.copytree('ros-indigo', overlay.repo_dir)
-shutil.copytree('ros-kinetic', overlay.repo_dir)
-shutil.copytree('ros-lunar', overlay.repo_dir)
+# shutil.copytree('ros-indigo', overlay.repo_dir)
+# shutil.copytree('ros-kinetic', overlay.repo_dir)
+# shutil.copytree('ros-lunar', overlay.repo_dir)
 
 # Commit changes and file pull request
 overlay.commit_changes()
-overlay.pull_request()
+overlay.pull_request(missing_deps)
