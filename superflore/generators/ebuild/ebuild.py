9560d5285bf06927ab2b6b9888acbb9eee0f0ead
Hunter L. Allen <hunter@openrobotics.org>
2017-12-20 16:31:25 -0600

    Strip spaces inserted in a comma separated list.

--- superflore/generators/ebuild/ebuild.py
+++ superflore/generators/ebuild/ebuild.py
@@ -123,7 +123,7 @@ class Ebuild(object):
             if len(split) > 1:
                 # they did something like "BSD,GPL,blah"
                 ret += 'LICENSE="( '
-                ret += ' '.join([get_license(l) for l in split])
+                ret += ' '.join([get_license(l.lstrip()) for l in split])
                 ret += ') "\n'
             else:
                 ret += "LICENSE=\""
