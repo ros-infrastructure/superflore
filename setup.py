2bb6056b2896791549b47fe058857000ff2073ef
Hunter Allen <hunter@openrobotics.org>
2017-12-28 16:08:11 -0600

    Bump version to 0.2.0 (#108)
    * Bump version to 0.2.0
    
    * Added CHANGELOG.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name='superflore',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(exclude=['tests']),
     author='Hunter L. Allen',
     author_email='hunter@openrobotics.org',
