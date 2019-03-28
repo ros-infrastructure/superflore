5069068ed6201af7df6a84b12ac42241e2b9424e
Andre Rosa <andre.rosa@lge.com>
2019-03-28 15:20:51 -0700

    Generate packagegroups/packagegroup-ros-world.bb

--- superflore/generators/bitbake/run.py
+++ superflore/generators/bitbake/run.py
@@ -149,6 +149,7 @@ def main():
                         err("No package to satisfy key '%s'" % pkg)
                         sys.exit(1)
                 yoctoRecipe.generate_rosdistro_conf(_repo, args.ros_distro, skip_keys)
+                yoctoRecipe.generate_packagegroup_ros_world(_repo, args.ros_distro)
                 # Commit changes and file pull request
                 regen_dict = dict()
                 regen_dict[args.ros_distro] = args.only
@@ -180,6 +181,7 @@ def main():
                 total_changes[distro] = distro_changes
                 total_installers[distro] = distro_installers
                 yoctoRecipe.generate_rosdistro_conf(_repo, args.ros_distro, skip_keys)
+                yoctoRecipe.generate_packagegroup_ros_world(_repo, args.ros_distro)
 
         num_changes = 0
         for distro_name in total_changes:
