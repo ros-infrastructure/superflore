2ea8e997cde85bae4c25e0c505bafc8f12cbe4b6
Andre Rosa <andre.rosa@lge.com>
2019-05-07 15:51:28 -0700

    Remove packagegroup-ros-world.bb as per OE recipe generation scheme rev 25

--- superflore/generators/bitbake/run.py
+++ superflore/generators/bitbake/run.py
@@ -153,8 +153,6 @@ def main():
                     _repo, args.ros_distro, overlay.get_file_revision_logs(
                         'files/{0}/cache.yaml'.format(args.ros_distro)),
                     distro.release_platforms, skip_keys)
-                yoctoRecipe.generate_packagegroup_ros_world(
-                    _repo, args.ros_distro)
                 yoctoRecipe.generate_distro_cache(_repo, args.ros_distro)
                 yoctoRecipe.generate_rosdep_resolve(_repo, args.ros_distro)
                 yoctoRecipe.generate_superflore_change_summary(
@@ -198,8 +196,6 @@ def main():
                     _repo, args.ros_distro, overlay.get_file_revision_logs(
                         'files/{0}/cache.yaml'.format(args.ros_distro)),
                     distro.release_platforms, skip_keys)
-                yoctoRecipe.generate_packagegroup_ros_world(
-                    _repo, args.ros_distro)
                 yoctoRecipe.generate_distro_cache(_repo, args.ros_distro)
                 yoctoRecipe.generate_rosdep_resolve(_repo, args.ros_distro)
                 yoctoRecipe.generate_superflore_change_summary(
