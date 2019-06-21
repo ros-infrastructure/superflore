c87a52e00d0d50d0ec3d545b339f048ca8beab0c
Andre Rosa <andre.rosa@lge.com>
2019-06-20 20:14:15 -0700

    Add an optional title parameter to the overlay pull_request method
      - Allows the called to customize the pull request title as needed

--- superflore/generators/bitbake/ros_meta.py
+++ superflore/generators/bitbake/ros_meta.py
@@ -66,9 +66,10 @@ class RosMeta(object):
         info('Committing to branch {0}...'.format(self.branch_name))
         self.repo.git.commit(m='{0}'.format(commit_msg))
 
-    def pull_request(self, message, distro=None):
-        pr_title = 'rosdistro sync, {0}'.format(time.ctime())
-        self.repo.pull_request(message, pr_title, branch=distro)
+    def pull_request(self, message, distro=None, title=''):
+        if not title:
+            title = 'rosdistro sync, {0}'.format(time.ctime())
+        self.repo.pull_request(message, title, branch=distro)
 
     def get_file_revision_logs(self, *file_path):
         return self.repo.git.log('--oneline', '--', *file_path)
